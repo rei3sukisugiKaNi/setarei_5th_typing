5705ebefc1d37b186a9580e734ec7141b5f4e83a
rei3sukisugiKaNi <setarei20200628.daisukiclub@gmail.com>
2025-05-29 21:21:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ progressContainer.style.cssText = `
   position: absolute;
   bottom: 5%;
   left: 5%;
-  width: 85%;
+  width: 90%;
   height: 6px;
   background-color: #eee;
   border-radius: 3px;
